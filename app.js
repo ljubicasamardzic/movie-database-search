17d5f163cfbdfc6ae7b545eaa2c2fcdeab86b52a
Ljubica Samardzic <ljubica.samardzic22@gmail.com>
2020-12-16 14:40:02 +0100

    initial formatting

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ function searchDB() {
 
     let searchURL = "http://www.omdbapi.com/?i=tt3896198&apikey=81094c1f" + "&s=" + term + "&type=" + type + "&y=" + year;
 
-
     $.ajax({
         type: "GET",
         url: searchURL,
@@ -25,20 +24,14 @@ function searchDB() {
                 let rowClass = title.replace(/[^\w\s]/gi, '').replace(/ /g, '');
                 // Remove special characters and replace spaces with hyphens - to make it distinct from class 
                 let idTitle = title.replace(/[^\w\s]/gi, '').replace(/ /g, "-");
-                // console.log('row class: ' + rowClass); 
-                // let titleWithoutSpecialChar = title.replace(/[^\w\s]/gi, '');
-                // console.log('id 1: ' + titleWithoutSpecialChar);
-                //  let titleClass = titleWithoutSpecialChar.replace(/ /g, "-");
-                //  console.log("title 1 woth char: " + titleClass);
-                
 
                 $('#movie-table').append(
-                    `
-                    <table cellpadding="22">
+                    `<div class="col-12 col-sm-3 col-md-2 single-table">
+                        <img src=${poster} width="150" height="180" alt="poster"></img>
+                    </div>
+                    <div class="col-12 col-sm-9 col-md-10 single-table">
+                    <table cellpadding="10">
                     <tr>
-                        <td rowspan="3">
-                            <img src=${poster} width="150" height="180" alt="poster"></img>
-                        </td>
                         <td>Naslov:</td>
                         <td>${title}</td>
                     </tr>
@@ -47,30 +40,25 @@ function searchDB() {
                         <td>${year}</td>
                     </tr>
                     <tr class="${rowClass}">
-                        <td>
-                            <button id="${idTitle}" data-attribute="${dataTitle}" class="btn btn-primary" onclick="findData(id)">Više informacija</button>
-                        </td>
-                        <td>
-                            <button class="btn btn-primary d-none" id="1${idTitle}" onclick="hideInfo(id)">Prikaži manje</button>
+                        <td colspan="2">
+                            <button id="${idTitle}" data-attribute="${dataTitle}" class="btn btn-primary btn-lg" onclick="findData(id)">Više informacija</button>
+                            <button class="btn btn-primary btn-lg d-none" id="1${idTitle}" onclick="hideInfo(id)">Prikaži manje</button>
                         </td>
                     </tr>
                     </table>
+                    </div>
+                   
                     `
-                    );
-
+                );
             });
-
-        }, 
-    error: (error) => console.log(error)
+        },
+        error: (error) => console.log(error)
     });
 
 }
 
-
 function findData(receivedId) {
-    // $('form').on('submit', (e) => {
-    //     e.preventDefault();
-    // });
+
     // Title with hyphens as received from the function
     let btnId = receivedId;
 
@@ -85,46 +73,40 @@ function findData(receivedId) {
     let tableRow = $('#movie-table tr.' + `${rowId}`);
     let searchUrl1 = "http://www.omdbapi.com/?i=tt3896198&apikey=81094c1f" + "&t=" + searchTitle;
 
+    $.ajax({
+        type: "GET",
+        url: searchUrl1,
+        success: (res) => {
+            console.log(res);
+            let dateReleased = res['Released'];
+            let runtime = res['Runtime'];
+            let director = res['Director'];
+            let actors = res['Actors'];
+            let plot = res['Plot'];
+            let seasonsNo = res['totalSeasons'];
 
-  $.ajax({
-      type: "GET",
-      url: searchUrl1,
-      success: (res) => {
-        console.log(res);
-          let dateReleased = res['Released'];
-          let runtime = res['Runtime'];
-          let director = res['Director'];
-          let actors = res['Actors'];
-          let plot = res['Plot'];
-          let seasonsNo = res['totalSeasons'];
+            let ratings = res['Ratings'];
+            let ratingSource = '';
+            let ratingValue = '';
+            ratings.forEach((rating) => {
+                ratingSource += (`<p>${rating['Source']}</p>`);
+                ratingValue += (`<p>${rating['Value']}</p>`);
+            });
+            let seasons = (seasonsNo !== undefined) ? `<tr class="2${btnId}"><td>Broj sezona:</td><td>${seasonsNo}</td></tr>` : '';
 
-          let ratings = res['Ratings'];
-          let ratingSource = '';
-          let ratingValue = '';
-          ratings.forEach((rating) => {
-              ratingSource += (`<p>${rating['Source']}</p>`);
-              ratingValue += (`<p>${rating['Value']}</p>`);
-          });
-          let seasons = (seasonsNo !== undefined)? `<tr class="2${btnId}"><td></td><td>Broj sezona:</td><td>${seasonsNo}</td></tr>` : '';
-
-          tableRow.before('<tr class="2'+`${btnId}`+'"><td>Datum objavljivanja:</td><td>' + `${dateReleased}` + '</td></tr><tr class="2'+`${btnId}`+'"><td></td><td>Trajanje:</td><td>' + `${runtime}` + '</td></tr><tr class="2'+`${btnId}`+'"><td></td><td>Režiser:</td><td>' + `${director}` + '</td></tr><tr class="2'+`${btnId}`+'"><td></td><td>Glumci:</td><td>' + `${actors}` + '</td></tr><tr class="2'+`${btnId}`+'"><td></td><td>Radnja:</td><td>' + `${plot}` + '</td></tr>' + `${seasons}` + '<tr class="2'+`${btnId}`+'"><td></td><td>Ocjene gledalaca:</td><td><table><tr><td>' + `${ratingSource}` + '</td><td>' + `${ratingValue}` + '</td></tr>');
-        //   tableRow.append(`<button class="btn btn-primary" id="1${btnId}" onclick="hideInfo(id)">Sakrij informacije</button>`)
-        $('button#' + `${btnId}`).css("visibility", "hidden");
-        let button2 = $('button#' + `${btnId}`).parent().siblings().children().first();
-        button2.removeClass('d-none');
+            tableRow.before('<tr class="2' + `${btnId}` + '"><td>Datum objavljivanja:</td><td>' + `${dateReleased}` + '</td></tr><tr class="2' + `${btnId}` + '"><td>Trajanje:</td><td>' + `${runtime}` + '</td></tr><tr class="2' + `${btnId}` + '"><td>Režiser:</td><td>' + `${director}` + '</td></tr><tr class="2' + `${btnId}` + '"><td>Glumci:</td><td>' + `${actors}` + '</td></tr><tr class="2' + `${btnId}` + '"><td>Radnja:</td><td>' + `${plot}` + '</td></tr>' + `${seasons}` + '<tr class="2' + `${btnId}` + '"><td>Ocjene gledalaca:</td><td><table><tr><td>' + `${ratingSource}` + '</td><td>' + `${ratingValue}` + '</td></tr>');
+            $('button#' + `${btnId}`).addClass('d-none');
+            $('button#' + `${btnId}`).siblings().removeClass('d-none');
 
         },
-          error: (error) => console.log(error)
-  });
-
-
+        error: (error) => console.log(error)
+    });
 };
-    
+
 function hideInfo(btnId2) {
     let plainId = btnId2.replace(1, '');
     let trId = '2' + `${plainId}`;
-    $('button#' + `${btnId2}`).parent().siblings().children().first().css('visibility', 'visible');
+    $('button#' + `${btnId2}`).siblings().removeClass('d-none');
     $('tr.' + `${trId}`).hide();
     $('button#' + `${btnId2}`).addClass('d-none');
-
 }
